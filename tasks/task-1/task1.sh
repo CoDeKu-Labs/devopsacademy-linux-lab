b35d7a0938f8a57b87a94773d9191ab9ecf41194
Nimesha Jinarajadasa <mhnimesh@gmail.com>
2025-02-02 23:43:00 +0530

    Update task1.sh

--- tasks/task-1/task1.sh
+++ tasks/task-1/task1.sh
@@ -3,7 +3,7 @@
 echo "📝 Task 1: Create a directory named 'myfolder' inside 'tasks/task1/'."
 
 # Define the expected directory path
-EXPECTED_DIR="$(pwd)/tasks/task1/myfolder"
+EXPECTED_DIR="$(pwd)/tasks/task-1/myfolder"
 
 # Check if the directory exists
 if [ -d "$EXPECTED_DIR" ]; then
